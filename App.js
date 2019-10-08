ee10cc0109bbe7e99abf5eac8a82d6006bc4e291
ethereousnatsudragneel <mnjt.singh2000@gmail.com>
2019-10-08 15:01:02 +0530

    app.js is the only file to be looked at

--- App.js
+++ App.js
@@ -1,40 +1,35 @@
 import React, {Component} from 'react'
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-import TabNav from './screens/tab.js'
-const initialState={up:require("./images/KeyboardKeyUp.png"),down:require("./images/KeyboardKeyDown.png"),right:require("./images/KeyboardKeyRight.png"),left:require("./images/KeyboardKeyLeft.png"),DRest:require("./images/DRest.png"),DWalk1:require("./images/DWalk1.png"),DWalk2:require("./images/DWalk2.png"),FRest:require("./images/FRest.png"),FWalk1:require("./images/FWalk1.png"),FWalk2:require("./images/FWalk2.png"),LRest:require("./images/LRest.png"),LWalk1:require("./images/LWalk1.png"),LWalk2:require("./images/LWalk2.png"),RRest:require("./images/RRest.png"),RWalk1:require("./images/RWalk1.png"),RWalk2:require("./images/RWalk2.png"),Tree:require("./images/Tree.png"),kekadu:require("./images/DRest.png"),Banana:require("./images/Banana.png"),Carrot:require("./images/Carrot.png"),Medicine:require("./images/Medicine.png"),
-DBird:require("./images/spriteart/DBird.png"),FBird:require("./images/spriteart/FBird.png"),LBird:require("./images/spriteart/LBird.png"),RBird:require("./images/spriteart/RBird.png"),DDonkey:require("./images/spriteart/DDonkey.png"),FDonkey:require("./images/spriteart/FDonkey.png"),LDonkey:require("./images/spriteart/LDonkey.png"),RDonkey:require("./images/spriteart/RDonkey.png"),DMonkey:require("./images/spriteart/DMonkey.png"),FMonkey:require("./images/spriteart/FMonkey.png"),LMonkey:require("./images/spriteart/LMonkey.png"),RMonkey:require("./images/spriteart/RMonkey.png"),DRabbit:require("./images/spriteart/DRabbit.png"),FRabbit:require("./images/spriteart/FRabbit.png"),LRabbit:require("./images/spriteart/LRabbit.png"),RRabbit:require("./images/spriteart/RRabbit.png"),DFox:require("./images/spriteart/DFox.png"),FFox:require("./images/spriteart/FFox.png"),LFox:require("./images/spriteart/LFox.png"),RFox:require("./images/spriteart/RFox.png"),
-BananaCount:0,CarrotCount:0,MedicineCount:0,x:160,y:160}
-const reducer=(state=initialState,action)=>{switch(action.type){
-case 'Go_Left1':
-return{x:304}
-case 'Go_FriendScreen':
-return{x:16}
-case 'kleft':
-return{x:state.x-16}
-case 'kright':
-return{x:state.x+16}
-case 'kup':
-return{y:state.y-16}
-case 'kdown':
-return{y:state.y+16}
-case 'Get_Banana':
-return {BananaCount:1}
-case 'Give_Banana':
-return {BananaCount:0}
-case 'Get_Carrot':
-return {CarrotCount:1}
-case 'Give_Carrot':
-return {CarrotCount:0}
-case 'Get_Medicine':
-return {MedicineCount:1}
-case 'Give_Medicine':
-return {MedicineCount:0}}
-return state}
-const store=createStore(reducer)
-class App extends Component{
-render(){return(
-<Provider store={store}>
-<TabNav/>
-</Provider>)}}
-export default App
+import {View,Button,Text,Image,TouchableOpacity,ImageBackground} from 'react-native'
+
+export default class FoxGame extends Component{
+constructor(props){
+super(props)
+this.state={wins:0,fox:require("./images/QuestionMark.png"),user:require("./images/QuestionMark.png"),random:null,jungle:require("./images/JungleBackgroundForGames.jpg"),rock:require("./images/Rock.png"),Paper:require("./images/Paper.png"),Scissor:require("./images/Scissor.png")}}
+generateFox=()=>{var x=Math.floor(Math.random()*100)+1
+this.setState({random:x%3})}
+rock=()=>{this.generateFox()
+if(this.state.random==2){this.setState(previousState=>({wins:previousState.wins+1,fox:previousState.Scissor}))}
+else if(this.state.random==1){this.setState(previousState=>({fox:previousState.Paper}))}
+else{this.setState(previousState=>({fox:previousState.Rock}))}
+this.setState(previousState=>({user:previousState.Rock}))}
+paper=()=>{this.generateFox()
+if(this.state.random==0){this.setState(previousState=>({wins:previousState.wins+1,fox:previousState.Rock}))}
+else if(this.state.random==1){this.setState(previousState=>({fox:previousState.Paper}))}
+else{rethis.setState(previousState=>({fox:previousState.Rock}))}
+this.setState(previousState=>({user:previousState.Paper}))}
+scissor=()=>{this.generateFox()
+if(this.state.random==1){this.setState(previousState=>({wins:previousState.wins+1,fox:previousState.Paper}))}
+else if(this.state.random==2){this.setState(previousState=>({fox:previousState.Scissor}))}
+else{this.setState(previousState=>({fox:previousState.Rock}))}
+this.setState(previousState=>({user:previousState.Scissor}))}
+render(){return(<ImageBackground source={this.state.jungle} style={{flex:1}}><View style={{height:'70%',width:'100%'}}>
+<Text style={{fontSize:25,textAlign:"center",top:25}}>ROCK PAPER SCISSOR</Text>
+<Text style={{fontSize:25,top:60,textAlign:"center"}}>{this.state.wins}</Text>
+<Image source={this.state.user} style={{height:128,width:128,left:64,top:160,position:'absolute'}}/>
+<Image source={this.state.fox} style={{left:256,top:160,height:128,width:128,position:'absolute'}}/>
+
+</View><View style={{flex:1,flexDirection:"row"}}>
+<View style={{flex:"stretch"}}><TouchableOpacity onPress={()=>this.rock()}><Image source={this.state.Rock}/></TouchableOpacity></View>
+<View style={{flex:"stretch"}}><TouchableOpacity onPress={()=>this.paper()}><Image source={this.state.Paper}/></TouchableOpacity></View>
+<View style={{flex:"stretch"}}><TouchableOpacity onPress={()=>this.scissor()}><Image source={this.state.Scissor}/></TouchableOpacity></View>
+</View></ImageBackground>)}}
