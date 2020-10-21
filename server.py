78a1df33c72383b3ea8f451e0c34b8cfd1e739e6
ethereousnatsudragneel <mnjt.singh2000@gmail.com>
2020-10-21 11:11:28 +0530

    Chat

--- server.py
+++ server.py
@@ -1,62 +1,39 @@
-#Server py
 import socket
 from _thread import *
 import sys
 
-server = "192.168.0.4"
-port = 5555
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server=socket.gethostbyname(socket.gethostname())
+port=5555
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 try:
-    s.bind((server, port))
+	s.bind((server,port))
 except socket.error as e:
-    str(e)
+	str(e)
 
 s.listen(2)
-print("Waiting for a connection, Server Started")
+print("Server up and kicking. Waiting for connections.")
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
+currentUser=0
+msgList=["",""]
 
+def threadedUser(conn,user):
+	global msgList, currentUser
+	while True:
+		data=conn.recv(1024).decode()
+		msgList[user]=data
+		if not data:
+			print("Disconnected")
+			break
+		else:
+			if user==1:
+				reply=msgList[0]
+			else:
+				reply=msgList[1]
+			conn.send(reply.encode())
 
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-pos = [(0,0),(100,100)]
-
-def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
-    reply = ""
-    while True:
-        try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
-
-            if not data:
-                print("Disconnected")
-                break
-            else:
-                if player == 1:
-                    reply = pos[0]
-                else:
-                    reply = pos[1]
-
-                print("Received: ", data)
-                print("Sending : ", reply)
-
-            conn.sendall(str.encode(make_pos(reply)))
-        except:
-            break
-
-    print("Lost connection")
-    conn.close()
-
-currentPlayer = 0
 while True:
-    conn, addr = s.accept()
-    print("Connected to:", addr)
-
-    start_new_thread(threaded_client, (conn, currentPlayer))
-    currentPlayer += 1
+	conn,addr=s.accept()
+	print("Connected to ",addr)
+	start_new_thread(threadedUser,(conn,currentUser))
+	currentUser+=1
